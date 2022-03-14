74dbc08281ea53393357ea940d501f8c3fa7efba
Sachin Hegde <sachinhegde2511@gmail.com>
2022-03-14 21:51:15 +0530

    first file

---
+++ string_comparision.php
@@ -0,0 +1,18 @@
+<?php
+
+$str1 = $_POST['str1'];
+$str2 = $_POST['str2'];
+for($i = 0; $i < strlen($str1); )
+{
+    if(($pos = strpos($str2, $str1[$i])) !== false)
+    {
+        $str1 = substr($str1, 0, $i) . substr($str1, $i + 1);
+        $str2 = substr($str2, 0, $pos) . substr($str2, $pos + 1);
+        continue;
+    }
+    $i++;
+}
+
+echo 'OUTPUT 1 : '.$str1."\n";
+echo 'OUTPUT 2 : '.$str2;
+?>
